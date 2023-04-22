288194789ceaec54236ef629abb2b51936c8a404
korysanan <ujin0561@naver.com>
2023-04-22 13:30:54 +0900

    Update test4.py

--- 연습해보는거/test4.py
+++ 연습해보는거/test4.py
@@ -1,96 +1,87 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLabel, QLineEdit, QPushButton, \
-    QTableWidget, QTableWidgetItem
-from typing import List, Tuple
-
-class ProcessorInput(QWidget):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-        self.process_label = QLabel('Process Name:', self)
-        self.at_label = QLabel('Arrival Time:', self)
-        self.workload_label = QLabel('Workload:', self)
-
-        self.process_input = QLineEdit(self)
-        self.at_input = QLineEdit(self)
-        self.workload_input = QLineEdit(self)
-
-        input_layout = QGridLayout()
-        input_layout.addWidget(self.process_label, 0, 0)
-        input_layout.addWidget(self.at_label, 1, 0)
-        input_layout.addWidget(self.workload_label, 2, 0)
-        input_layout.addWidget(self.process_input, 0, 1)
-        input_layout.addWidget(self.at_input, 1, 1)
-        input_layout.addWidget(self.workload_input, 2, 1)
-
-        self.setLayout(input_layout)
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem
 
 
-class FCFS(QWidget):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.process_input_group = QGroupBox('Process Input', self)
-        self.table_group = QGroupBox('Process Schedule', self)
+        # Create widgets for process input
+        self.process_name_edit = QLineEdit()
+        self.arrival_time_edit = QLineEdit()
+        self.workload_edit = QLineEdit()
+        self.add_process_button = QPushButton("Add Process")
+        self.add_process_button.clicked.connect(self.add_process)
 
-        self.process_input_layout = QHBoxLayout()
-        self.table_layout = QVBoxLayout()
+        # Create table widget for process data
+        self.process_table = QTableWidget()
+        self.process_table.setColumnCount(3)
+        self.process_table.setHorizontalHeaderLabels(["Process Name", "Arrival Time", "Workload"])
 
-        self.process_input_layout.addWidget(self.process_input_group)
-        self.process_input_layout.addWidget(self.table_group)
+        # Create table widget for FCFS results
+        self.fcfs_table = QTableWidget()
+        self.fcfs_table.setColumnCount(6)
+        self.fcfs_table.setHorizontalHeaderLabels(["Process", "AT", "BT", "WT", "TT", "NTT"])
 
-        self.process_input_group.setLayout(ProcessorInput(self))
-        self.table_group.setLayout(QTableWidget(self))
+        # Add widgets to layout
+        process_input_layout = QHBoxLayout()
+        process_input_layout.addWidget(QLabel("Process Name:"))
+        process_input_layout.addWidget(self.process_name_edit)
+        process_input_layout.addWidget(QLabel("Arrival Time:"))
+        process_input_layout.addWidget(self.arrival_time_edit)
+        process_input_layout.addWidget(QLabel("Workload:"))
+        process_input_layout.addWidget(self.workload_edit)
+        process_input_layout.addWidget(self.add_process_button)
 
-        self.table_layout.addWidget(self.table_group)
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(process_input_layout)
+        main_layout.addWidget(self.process_table)
+        main_layout.addWidget(self.fcfs_table)
 
-        self.setLayout(self.table_layout)
+        self.setLayout(main_layout)
 
-    def add_row(self):
-        table = self.table_group.layout().itemAt(0).widget()
-        process = self.process_input_group.layout().itemAt(0).widget().process_input.text()
-        at = int(self.process_input_group.layout().itemAt(0).widget().at_input.text())
-        workload = int(self.process_input_group.layout().itemAt(0).widget().workload_input.text())
+    def add_process(self):
+        # Get input values from line edits
+        process_name = self.process_name_edit.text()
+        arrival_time = self.arrival_time_edit.text()
+        workload = self.workload_edit.text()
 
-        row = table.rowCount()
-        table.insertRow(row)
-        table.setItem(row, 0, QTableWidgetItem(process))
-        table.setItem(row, 1, QTableWidgetItem(str(at)))
-        table.setItem(row, 2, QTableWidgetItem(str(workload)))
+        # Add process data to process table
+        row_position = self.process_table.rowCount()
+        self.process_table.insertRow(row_position)
+        self.process_table.setItem(row_position, 0, QTableWidgetItem(process_name))
+        self.process_table.setItem(row_position, 1, QTableWidgetItem(arrival_time))
+        self.process_table.setItem(row_position, 2, QTableWidgetItem(workload))
 
-    def schedule_processes(self):
-        table = self.table_group.layout().itemAt(0).widget()
+        # Calculate FCFS values
+        if row_position == 0:
+            wt = 0
+            tt = int(workload)
+            ntt = 1
+        else:
+            previous_row = row_position - 1
+            previous_tt = int(self.fcfs_table.item(previous_row, 4).text())
+            previous_at = int(self.fcfs_table.item(previous_row, 1).text())
+            previous_bt = int(self.fcfs_table.item(previous_row, 2).text())
+            current_at = int(arrival_time)
+            if current_at > previous_tt:
+                wt = 0
+            else:
+                wt = previous_tt - current_at
+            tt = wt + int(workload)
+            ntt = tt / int(workload)
 
-        processes = []
-        for i in range(table.rowCount()):
-            processes.append({
-                'name': table.item(i, 0).text(),
-                'at': int(table.item(i, 1).text()),
-                'workload': int(table.item(i, 2).text())
-            })
+        # Add calculated values to FCFS table
+        self.fcfs_table.insertRow(row_position)
+        self.fcfs_table.setItem(row_position, 0, QTableWidgetItem(process_name))
+        self.fcfs_table.setItem(row_position, 1, QTableWidgetItem(arrival_time))
+        self.fcfs_table.setItem(row_position, 2, QTableWidgetItem(workload))
+        self.fcfs_table.setItem(row_position, 3, QTableWidgetItem(str(wt)))
+        self.fcfs_table.setItem(row_position, 4, QTableWidgetItem(str(tt)))
+        self.fcfs_table.setItem(row_position, 5, QTableWidgetItem(str(ntt)))
 
-        processes.sort(key=lambda x: x['at'])
-
-        current_time = 0
-        for i in range(len(processes)):
-            process = processes[i]
-            bt = process['workload']
-            wt = current_time - process['at']
-            tt = wt + bt
-            ntt = tt / bt
-
-            current_time += bt
-
-            row = table.rowCount()
-            table.insertRow(row)
-            table.setItem(row, 0, QTableWidgetItem(process['name']))
-            table.setItem(row, 1, QTableWidgetItem(str(process['at'])))
-            table.setItem(row, 2, QTableWidgetItem(str(bt)))
-            table.setItem(row, 3, QTableWidgetItem(str(wt)))
-            table.setItem(row, 4, QTableWidgetItem(str(tt)))
-            table.setItem(row, 5, QTableWidgetItem(str(ntt)))
 
 if __name__ == "__main__":
     app = QApplication([])
-    processor_input = ProcessorInput()
-    processor_input.show()
-    app.exec_()        
+    window = MainWindow()
+    window.show()
+    app.exec_()
