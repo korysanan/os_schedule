d5b57746142e9c5226beedb2f6533a8c6f97d6eb
korysanan <ujin0561@naver.com>
2023-05-06 14:41:56 +0900

    예시 코드 기준 quantum = 2일때 제외하고는 작동하긴함
    근데 2일 때는 왜 계속 오류일까

--- RR_Scheduling.py
+++ RR_Scheduling.py
@@ -4,16 +4,17 @@ def RR(n, processes, quantum):
     waiting_time = [0]*n
     turnaround_time = [0]*n
     current_time = 0
+    queue = []
 
     while True:
         done = True
         for i in range(n):
-            if remaining_burst_time[i] > 0:
+            if processes[i][1] <= current_time and remaining_burst_time[i] > 0:
                 done = False
-
                 if remaining_burst_time[i] > quantum:
                     current_time += quantum
                     remaining_burst_time[i] -= quantum
+                    queue.append(i)
                 else:
                     current_time += remaining_burst_time[i]
                     completion_time[i] = current_time
@@ -21,7 +22,13 @@ def RR(n, processes, quantum):
                     turnaround_time[i] = completion_time[i] - processes[i][1]
                     remaining_burst_time[i] = 0
                     processes[i] = processes[i] + [waiting_time[i], turnaround_time[i]]
-        
+                    while len(queue) > 0 and remaining_burst_time[queue[0]] == 0:
+                        j = queue.pop(0)
+                        completion_time[j] = current_time
+                        waiting_time[j] = completion_time[j] - processes[j][1] - processes[j][2]
+                        turnaround_time[j] = completion_time[j] - processes[j][1]
+                        processes[j] = processes[j] + [waiting_time[j], turnaround_time[j]]
+
         if done == True:
             break
 
