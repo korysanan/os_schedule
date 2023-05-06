500d60e26729faee7182a9724a2310bb3186add3
korysanan <ujin0561@naver.com>
2023-05-06 15:31:21 +0900

    ui 수정

--- 참고할것들/chat_gpt 이용/RR.py
+++ 참고할것들/chat_gpt 이용/RR.py
@@ -1,45 +1,38 @@
-# 프로세스 정보를 담은 리스트
-processes = []
-# 프로세스의 개수
-n = int(input("프로세스의 개수를 입력하세요: "))
-# 시간 할당량
-time_quantum = int(input("시간 할당량을 입력하세요: "))
-# 각 프로세스의 정보를 입력받아 리스트에 추가
-for i in range(n):
-    print("프로세스 ", i+1)
-    arrival_time = int(input("도착 시간을 입력하세요: "))
-    burst_time = int(input("실행 시간을 입력하세요: "))
-    processes.append((arrival_time, burst_time))
-
-# 실행 순서와 각 프로세스의 실행 완료 시간, 대기 시간을 계산
-completion_time = [0]*n
-waiting_time = [0]*n
-turnaround_time = [0]*n
-remaining_time = [burst_time for arrival_time, burst_time in processes]
-remaining_processes = n
-current_time = 0
-while remaining_processes > 0:
+def round_robin(processes, quantum):
+    n = len(processes)
+    waiting_time = [0] * n
+    turnaround_time = [0] * n
+    remaining_burst_time = [processes[i][2] for i in range(n)]
+    time = 0
+    done = [False] * n
+    
+    while True:
+        done_count = 0
+        for i in range(n):
+            if remaining_burst_time[i] > 0:
+                if remaining_burst_time[i] > quantum:
+                    time += quantum
+                    remaining_burst_time[i] -= quantum
+                else:
+                    time += remaining_burst_time[i]
+                    waiting_time[i] = time - processes[i][1] - processes[i][2]
+                    remaining_burst_time[i] = 0
+                    turnaround_time[i] = time - processes[i][1]
+                    done[i] = True
+                    done_count += 1
+        
+        if done_count == n:
+            break
+    
+    avg_waiting_time = sum(waiting_time) / n
+    avg_turnaround_time = sum(turnaround_time) / n
+    
     for i in range(n):
-        if remaining_time[i] > time_quantum:
-            current_time += time_quantum
-            remaining_time[i] -= time_quantum
-        elif remaining_time[i] > 0:
-            current_time += remaining_time[i]
-            completion_time[i] = current_time
-            waiting_time[i] = completion_time[i] - processes[i][0] - processes[i][1]
-            turnaround_time[i] = completion_time[i] - processes[i][0]
-            remaining_time[i] = 0
-            remaining_processes -= 1
+        print("Process:", processes[i][0][0], "WT:", waiting_time[i], "TT:", turnaround_time[i])
+    print("Average WT:", avg_waiting_time)
+    print("Average TT:", avg_turnaround_time)
 
-# 평균 대기 시간과 평균 회전 시간을 계산
-avg_waiting_time = sum(waiting_time)/n
-avg_turnaround_time = sum(turnaround_time)/n
+processes = [["p1", 0, 3], ["p2", 1, 7], ["p3", 3, 2], ["p4", 5, 5], ["p5", 6, 3]]
+quantum = 2
 
-# 결과 출력
-print("\nRR 스케줄링 결과")
-print(f"시간 할당량: {time_quantum}")
-print("프로세스\t도착 시간\t실행 시간\t완료 시간\t대기 시간\t회전 시간")
-for i in range(n):
-    print(f"{i+1}\t\t{processes[i][0]}\t\t{processes[i][1]}\t\t{completion_time[i]}\t\t{waiting_time[i]}\t\t{turnaround_time[i]}")
-print(f"\n평균 대기 시간: {avg_waiting_time}")
-print(f"평균 회전 시간: {avg_turnaround_time}")
+round_robin(processes, quantum)
