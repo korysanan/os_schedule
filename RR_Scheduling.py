3acc1ccb5df83b10949f86e2e99d509fa8921838
korysanan <ujin0561@naver.com>
2023-05-08 08:57:06 +0900

    ui 수정 및 색 추가

--- RR_Scheduling.py
+++ RR_Scheduling.py
@@ -16,19 +16,24 @@ def RR(n, processes, quantum):
                     remaining_burst_time[i] -= quantum
                     queue.append(i)
                 else:
-                    current_time += remaining_burst_time[i]
-                    completion_time[i] = current_time
-                    waiting_time[i] = completion_time[i] - processes[i][1] - processes[i][2]
-                    turnaround_time[i] = completion_time[i] - processes[i][1]
-                    remaining_burst_time[i] = 0
-                    processes[i] = processes[i] + [waiting_time[i], turnaround_time[i]]
-                    while len(queue) > 0 and remaining_burst_time[queue[0]] == 0:
-                        j = queue.pop(0)
-                        completion_time[j] = current_time
-                        waiting_time[j] = completion_time[j] - processes[j][1] - processes[j][2]
-                        turnaround_time[j] = completion_time[j] - processes[j][1]
-                        processes[j] = processes[j] + [waiting_time[j], turnaround_time[j]]
-
+                    if remaining_burst_time[i] <= quantum: # 추가된 if문
+                        current_time += remaining_burst_time[i]
+                        completion_time[i] = current_time
+                        waiting_time[i] = completion_time[i] - processes[i][1] - processes[i][2]
+                        turnaround_time[i] = completion_time[i] - processes[i][1]
+                        remaining_burst_time[i] = 0
+                        processes[i] = processes[i] + [waiting_time[i], turnaround_time[i]]
+                        while len(queue) > 0 and remaining_burst_time[queue[0]] == 0:
+                            j = queue.pop(0)
+                            completion_time[j] = current_time
+                            waiting_time[j] = completion_time[j] - processes[j][1] - processes[j][2]
+                            turnaround_time[j] = completion_time[j] - processes[j][1]
+                            processes[j] = processes[j] + [waiting_time[j], turnaround_time[j]]
+                    else:
+                        current_time += quantum # p[i][2] > quantum 인 경우
+                        remaining_burst_time[i] -= quantum
+                        queue.append(i)
+                    
         if done == True:
             break
 
